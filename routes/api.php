42c5c450f98705f35607dac6923162a474092baa
wzhyyds123 <86219629+wzhyyds123@users.noreply.github.com>
2022-06-01 16:10:44 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('11',[testcontroller::class,'text']);
+Route::any('11',[testcontroller::class,'text']);
