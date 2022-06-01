595ce7ede9c8462811feb8c4d736d73486a8d8c5
wzhyyds123 <86219629+wzhyyds123@users.noreply.github.com>
2022-06-01 10:00:56 +0800

    Update testcontroller.php

--- app/Http/Controllers/testcontroller.php
+++ app/Http/Controllers/testcontroller.php
@@ -2,15 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 
 class testcontroller extends Controller
 {
-   public function text(Request $request)
+   public function text()
    {
-       $email = $request->input('email');
-       $text = "123";
-       $subject = "无主题i";
-       $re = sendemail($email,$subject,$text);
+       $a = 1;
+    dd($a);
    }
 }
